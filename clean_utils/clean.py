e137825ff4d78afb3f3a05aee24fd7af5ac88162
Emanuele Roppo <ema.roppo@gmail.com>
2022-12-16 16:50:57 +0100

    Added new cleaning functions

--- clean_utils/clean.py
+++ clean_utils/clean.py
@@ -1,10 +1,17 @@
-import re
 import os
-from clean_utils.regex_dict import regex_dict
+import re
+
 from pymongo import MongoClient
+
+from clean_utils.regex_dict import regex_dict
 
 client = MongoClient("localhost", 27017)
 db = client.email
+
+
+def remove_corrupted_characters(body):
+    body = re.sub(r"(=09|=20|=01|=\n)", r"", body)
+    return body
 
 
 def strip_body(thread_dict):
@@ -33,12 +40,21 @@ def remove_gt(body):
     return new_body
 
 
-x = db.enron_dataset.find_one({"head_message.body": {"$regex": "\n>"}})
-print(x["head_message"]["body"])
+def remove_img_token(body):
+    return body.replace("[IMAGE]", "").strip()
 
-x["head_message"]["body"] = remove_gt(x["head_message"]["body"])
-print(strip_body(x))
 
-print(x["head_message"]["body"])
+def retrieve_subject(body):
+    subj_match = re.match(r"Subject:(.*)\n", body, re.IGNORECASE)
+
+    if subj_match:
+        subject = subj_match.group(1).strip()
+        body = re.sub(r"Subject:(.*)\n", "", body)
+
+        return body, subject
+
+    else:
+        return body, None
+
 
 # remove signature functions are useless duplicates now, reminder to self to remove one (or both?) :0
