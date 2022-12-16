21d91251da36fe21926148443b41d29d79c59ca4
Emanuele Roppo <ema.roppo@gmail.com>
2022-12-16 22:58:38 +0100

    Added options to filter and balance dataset, changed template for the prompt

--- db_utils/gen_dataset.py
+++ db_utils/gen_dataset.py
@@ -1,10 +1,11 @@
+import os
+from copy import deepcopy
+
 from pymongo import MongoClient
 from tqdm import tqdm
-import os
-from clean_utils.clean import strip_body, remove_corrupted_characters, remove_gt
 
 
-def create_txt(email, to_file=False, txt_path=None):
+def create_txt(email, to_file=False, txt_path=None, simplified_prompt=True):
     if "tags" in email:
         tags = list(set(email["tags"]))
         tags.sort()
@@ -12,10 +13,17 @@ def create_txt(email, to_file=False, txt_path=None):
         tags = []
 
     body = email["body"]
-    body = remove_gt(remove_corrupted_characters(body)).strip()
-    prompt_model = (
-        f"""[TAGS_START]{",".join(tags)}[TAGS_END][BODY_START]{body}[BODY_END]"""
-    )
+
+    if simplified_prompt:
+        prompt_model = body
+
+        if "tags" in email:
+            for i in email["tags"]:
+                prompt_model = i + prompt_model
+    else:
+        prompt_model = (
+            f"""[TAGS_START]{",".join(tags)}[TAGS_END][BODY_START]{body}[BODY_END]"""
+        )
 
     if to_file and body.strip() != "":
         with open(txt_path, "w") as f:
@@ -23,21 +31,75 @@ def create_txt(email, to_file=False, txt_path=None):
     return prompt_model
 
 
-client = MongoClient("localhost", 27017)
-db = client.email
-
-
-def create_dataset(db, target_dir):
+def create_dataset(
+    db,
+    target_dir,
+    scope="head_message",
+    min_len=3,
+    max_len=400,
+    exclude_html=True,
+    balance_dataset=False,
+):
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
-    cursor = db.enron_dataset.find()
-    for i, j in tqdm(enumerate(cursor)):
-        create_txt(j["head_message"], to_file=True, txt_path=f"{target_dir}/{i}.txt")
-        for k, l in enumerate(j["messages"]):
-            create_txt(l, to_file=True, txt_path=f"{target_dir}/{i}_{k}.txt")
+    filter = []
+    if exclude_html:
+        filter.append({"head_message.is_html": {"$exists": False}})
 
+    if min_len:
+        filter.append({"head_message.word_count": {"$gte": min_len}})
 
-create_dataset(db, "email_bodies")
+    if max_len:
+        filter.append({"head_message.word_count": {"$lte": max_len}})
 
-client.close()
+    if len(filter) == 1:
+        filter = filter[0]
+
+    if len(filter) > 1:
+        filter = {"$and": filter}
+
+    if len(filter) > 0:
+        cursor = db.enron_dataset.find(filter)
+    else:
+        cursor = db.enron_dataset.find()
+
+    if balance_dataset:
+        tagged_documents_count = db.enron_dataset.count_documents(
+            {"head_message.tags": {"$exists": True}}
+        )
+        tagged_documents_filter = deepcopy(filter)
+        tagged_documents_filter["$and"] += [{"head_message.tags": {"$exists": True}}]
+
+        tagged_documents_cursor = db.enron_dataset.find(tagged_documents_filter)
+        for i, j in tqdm(enumerate(tagged_documents_cursor)):
+            create_txt(
+                j["head_message"],
+                to_file=True,
+                txt_path=f"{target_dir}/{i}_tagged.txt",
+            )
+        untagged_documents_filter = deepcopy(filter)
+        untagged_documents_filter["$and"] += [{"head_message.tags": {"$exists": False}}]
+        untagged_documents_cursor = db.enron_dataset.find(
+            untagged_documents_filter
+        ).limit(tagged_documents_count)
+
+        for i, j in tqdm(enumerate(untagged_documents_cursor)):
+            create_txt(
+                j["head_message"],
+                to_file=True,
+                txt_path=f"{target_dir}/{i}_untagged.txt",
+            )
+
+    else:
+
+        cursor = db.enron_dataset.find()
+
+        for i, j in tqdm(enumerate(cursor)):
+            create_txt(
+                j["head_message"], to_file=True, txt_path=f"{target_dir}/{i}.txt"
+            )
+
+            if scope == "messages" or scope == "all":
+                for k, l in enumerate(j["messages"]):
+                    create_txt(l, to_file=True, txt_path=f"{target_dir}/{i}_{k}.txt")
